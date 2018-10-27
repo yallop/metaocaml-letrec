bcdc30aff41b34176cc666a356c2b92c142ac53c
Jeremy Yallop <yallop@gmail.com>
2018-10-27 13:26:10 +0100

    Add support for OCaml 4.07.1+BER

---
+++ myocamlbuild.ml
@@ -0,0 +1,13 @@
+open Ocamlbuild_plugin;;
+open Ocamlbuild_pack;;
+
+dispatch begin
+  function
+  | After_rules ->
+     flag ["ocaml"; "compile"; "open_print_code"] &
+     if Sys.ocaml_version = "4.04.0" then S[A"-open"; A"Print_code"]
+     else if Sys.ocaml_version = "4.07.1" then S[A"-open"; A"Codelib"]
+     else Printf.kprintf failwith "Unsupported OCaml version %s" Sys.ocaml_version
+  | _ -> ()
+end;;
+
